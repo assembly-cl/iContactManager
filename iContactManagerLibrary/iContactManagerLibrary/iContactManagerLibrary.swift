7f71c5d33bdbc27c2a5ad0ecd16d2ef315d46b68
rodor <rerazo@assembly.cl>
2020-05-09 09:53:13 -0400

    coding begins..

---
+++ iContactManagerLibrary/iContactManagerLibrary/iContactManagerLibrary.swift
@@ -0,0 +1,11 @@
+//
+//  iContactManagerLibrary.swift
+//  iContactManagerLibrary
+//
+//  Created by rodor on 09-05-20.
+//  Copyright © 2020 Assembly Chile. All rights reserved.
+//
+
+class iContactManagerLibrary {
+
+}
